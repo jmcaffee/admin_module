12b227fa62f506651376cd9232289cce993a7678
Jeff McAffee <jeff@ktechsystems.com>
2013-11-22 12:04:09 -0700

    Correct typo
    	modified:   spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 if ENV['coverage']
-  raise 'simplecov only works on Ruby 1.0' unless RUBY_VERSION =~ /^1\.9/
+  raise 'simplecov only works on Ruby 1.9' unless RUBY_VERSION =~ /^1\.9/
 
   require 'simplecov'
   SimpleCov.start { add_filter "spec/" }
