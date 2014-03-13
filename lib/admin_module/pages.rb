d3ca86dc0965a6f48cd58252686fb6bd6344f0db
Jeff McAffee <jeff@ktechsystems.com>
2014-03-13 12:10:23 -0700

    Prevent chromedriver from displaying non-fatal messages
    	modified:   lib/admin_module/pages.rb

--- lib/admin_module/pages.rb
+++ lib/admin_module/pages.rb
@@ -44,6 +44,7 @@ private
     caps = Selenium::WebDriver::Remote::Capabilities.chrome
     #caps['chromeOptions'] = {'binary' => '/opt/bin/test/chrome-27.0.1453.94.exe' }
     # See http://peter.sh/experiments/chromium-command-line-switches/ for a list of available switches.
+    # See https://sites.google.com/a/chromium.org/chromedriver/capabilities for details on setting ChromeDriver caps.
 
     # NOTE: The only way I've found to stop the EULA from being displayed is to
     # use the user-data-dir switch and point to a dir where chrome can put the
@@ -55,7 +56,12 @@ private
     user_data_dir = File.expand_path('test/chrome-data')
     #puts "*** user_data_dir location: #{user_data_dir}"
 
-    switches = %w[--ignore-certificate-errors --disable-popup-blocking --disable-translate --no-first-run]
+    # ignore-certificate-errors:  Ignores certificate-related errors.
+    # disable-popup-blocking:     Disable pop-up blocking.
+    # disable-translate:          Allows disabling of translate from the command line to assist with automated browser testing
+    # no-first-run:               Skip First Run tasks, whether or not it's actually the First Run.
+    # log-level:                  Sets the minimum log level. Valid values are from 0 to 3: INFO = 0, WARNING = 1, LOG_ERROR = 2, LOG_FATAL = 3.
+    switches = %w[--ignore-certificate-errors --disable-popup-blocking --disable-translate --no-first-run --log-level=3]
     switches << "--user-data-dir=#{user_data_dir}"
 
     # Create a client so we can adjust the timeout period.
