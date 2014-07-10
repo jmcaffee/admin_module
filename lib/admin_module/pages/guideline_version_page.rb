0d0dfd8245fd1d5995ee59839d5ac180f1bd515c
Jeff McAffee <jeff@ktechsystems.com>
2014-07-10 12:31:32 -0700

    Account for HTML encoding when comparing version comments
    	modified:   lib/admin_module/pages/guideline_version_page.rb

--- lib/admin_module/pages/guideline_version_page.rb
+++ lib/admin_module/pages/guideline_version_page.rb
@@ -129,6 +129,7 @@ EOS
     # The entire 1st version row (TR) element:
     version_row = doc.css("#dgrVersions > tbody > tr:nth-child(2)")
 
+    # We have to account for HTML encodings when comparing comments.
     unless version_row.to_s.include?(CGI.escapeHTML(comments))
       add_error("Version upload not completed. Comment not found.")
     end
