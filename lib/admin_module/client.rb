4588c2704036c2c0d1758fc5b5fe751546bc3add
Jeff McAffee <jeff@ktechsystems.com>
2014-07-17 08:22:39 -0700

    Add Client class and spec

---
+++ lib/admin_module/client.rb
@@ -0,0 +1,98 @@
+##############################################################################
+# File::    client.rb
+# Purpose:: AdminModule client object
+# 
+# Author::    Jeff McAffee 07/11/2014
+# Copyright:: Copyright (c) 2014, kTech Systems LLC. All rights reserved.
+# Website::   http://ktechsystems.com
+##############################################################################
+
+module AdminModule
+  class Client
+
+    attr_writer :page_factory
+
+    #
+    # Override credentials
+    #
+
+    attr_writer :user
+    attr_writer :password
+
+    def env=(environment)
+      AdminModule.configuration.current_env = environment
+    end
+
+    def env
+      AdminModule.configuration.current_env
+    end
+
+    def guideline
+      login
+      Guideline.new page_factory
+    end
+
+    def page_factory
+      @page_factory ||= AdminModule::PageFactory.new
+    end
+
+    ##
+    # Login to the admin module.
+    #
+    # If no credentials are provided, try to get credentials from the config object.
+    #
+
+    def login(user = nil, pass = nil)
+      if @logged_in
+        return true
+      end
+
+      user, pass = verify_credentials user, pass
+
+      logout
+      page_factory.login_page(true).login_as(user, pass)
+      @logged_in = true
+    end
+
+    ##
+    # Logout of the admin module
+    #
+
+    def logout
+      page_factory.login_page(true).logout
+      @logged_in = false
+    end
+
+  private
+
+    ##
+    # If credential args are empty, attempt to look them up,
+    #  first in the client attributes, then in the config obj.
+
+    def verify_credentials user, pass
+      return [user, pass] if valid_user_and_pass? user, pass
+
+      # Pull values stored in this client.
+      user, pass = @user, @password
+      return [user, pass] if valid_user_and_pass? user, pass
+
+      # Pull values stored in the config.
+      user, pass = AdminModule.configuration.user_credentials
+      return [user, pass] if valid_user_and_pass? user, pass
+
+      fail AuthenticationRequired.new("Missing credentials for #{env}")
+    end
+
+    def valid_user_and_pass? user, pass
+      if user.nil? || user.empty?
+        return false
+      end
+
+      if pass.nil? || pass.empty?
+        return false
+      end
+
+      true
+    end
+  end # Client
+end # AdminModule
