11f40eb0a78535470327deed8c3f863d33bda16f
Jeff McAffee <jeff.mcaffee@bkfs.com>
2015-06-27 12:29:21 -0700

    Attempt to speed up activation of PPMs
    	modified:   lib/admin_module/pages/ppms_page.rb

--- lib/admin_module/pages/ppms_page.rb
+++ lib/admin_module/pages/ppms_page.rb
@@ -82,9 +82,49 @@ class PpmsPage
   end
 
   def set_ppms_data data
+    avail_ppms = get_available_ppms
+    active_ppms = get_active_ppms
+    ppms_to_remove = Array.new
+    ppms_to_add = Array.new
+    working_set = data.dup
+
+    active_ppms.each_index do |i|
+      if working_set.include? active_ppms[i]
+        working_set.delete active_ppms[i]
+      else
+        ppms_to_remove << i
+      end
+    end
+
+    avail_ppms.each_index do |i|
+      if working_set.include? avail_ppms[i]
+        ppms_to_add << i
+        working_set.delete avail_ppms[i]
+      end
+    end
+
+    ppms_to_remove.each do |i|
+      paramters_selected_element.options[i].click
+    end
+    self.remove_parameters_button
+
+    ppms_to_add.each do |i|
+      parameters_available_element.options[i].click
+    end
+    self.add_parameters_button
+
+    self
+  end
+
+  def set_ppms_data_deprecated data
     self.remove_all_parameters_button
     assert_all_fields_removed self.parameters_selected_options, 'Parameters'
 
+    parameters_available_element.options.each do |opt|
+      if data.include? opt.text
+        opt.click
+      end
+    end
     data.each do |p|
       parameters_available_element.select(p)
       self.add_parameters_button
